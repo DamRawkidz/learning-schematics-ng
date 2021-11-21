ad147d85b8b00ab7689d202be7ca68851ee9cdb3
rawkidz@hotmail.com <rawkidz@hotmail.com>
2021-11-22 04:50:29 +0700

    [init] - start lerating

---
+++ src/order-wizard/index.ts
@@ -0,0 +1,34 @@
+
+import {
+  apply,
+  MergeStrategy,
+  mergeWith,
+  move,
+  Rule,
+  SchematicContext,
+  template,
+  Tree,
+  url
+} from '@angular-devkit/schematics';
+import { normalize, strings } from '@angular-devkit/core';
+
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export function orderWizard(_options: any): Rule {
+  console.log(_options); 
+  return (tree: Tree, _context: SchematicContext) => {
+    const folderPath = normalize(strings.dasherize(`${_options.path}/${_options.name}`))
+    let files = url('./files') 
+    const newTree = apply(files,[
+      move(folderPath),
+      template({
+          ...strings,
+          ..._options
+        })
+    ])
+  
+    const templateRule = mergeWith(newTree,MergeStrategy.Default)
+    return templateRule(tree, _context);
+  };
+}
